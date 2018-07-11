169a2ce7f1e7677ed9184e20ae52e9d153c34435
kaizhou <zhoukaisspu@163.com>
2018-07-11 15:42:26 +0800

    Add snp protocol to mointor communicationAdd

--- test/serialTest/serialTest.cpp
+++ test/serialTest/serialTest.cpp
@@ -5,12 +5,101 @@
 #include "SerialAdapter.h"
 #include <iostream>
 #include <numeric>
+#include <vector>
+#include <optional>
 
+class DataProtocl{
+public:
+	DataProtocl(){
+		payIndex = 0x00;
+		mType = DataType::SOF;	
+	}
+struct format{
+	unsigned short length;
+	unsigned char cmd0;
+	unsigned char cmd1;
+	std::vector<unsigned char> payload;
+};
+std::optional<format> parseData(unsigned char dat)
+{
+	switch(mType){
+		case DataType::SOF:
+			if(dat == 0xfe){
+				mType = DataType::LENGTH_LOW;
+				payIndex = 0x00;
+				mProData.payload.clear();
+			}
+		break;
+		case DataType::LENGTH_LOW:
+			mProData.length = dat;
+			mType = DataType::LENGTH_HIGH;
+		break;
+		case DataType::LENGTH_HIGH:
+			mProData.length = ((dat << 8) | mProData.length); 
+			mType = DataType::CMD0;
+		break;
+		case DataType::CMD0:
+			mProData.cmd0 = dat;
+			mType = DataType::CMD1;	
+		break;
+		case DataType::CMD1:
+			mProData.cmd1 = dat;
+			if (mProData.length == 0)
+				mType = DataType::FCS;
+			else
+				mType = DataType::PAYLOAD;
+		break;
+		case DataType::PAYLOAD:
+			mProData.payload.push_back(dat);
+			payIndex++;
+			if(payIndex ==  mProData.length)
+				mType = DataType::FCS;
+		break;
+		case DataType::FCS:
+			if(dat == exclusive_or(mProData)){
+				mType = DataType::SOF;
+				payIndex = 0x00;
+				return std::optional<format>(mProData);
+			 } else {
+			 	payIndex = 0x00;
+				mType = DataType::SOF;
+			 }
+		break;
+		default:
+		break;
+	}
+	return std::nullopt;
+}
+private:
+unsigned char exclusive_or(format& data)
+{
+	unsigned char res = 0x00;
+	res = res ^ (data.length & 0x00ff);
+	res = res ^ ((data.length & 0xff00)>>8);
+	res = res ^ data.cmd0;
+	res = res ^ data.cmd1;
+	for(auto& n : data.payload){
+		res = res ^ n;
+	}
+	return res;
+}
+enum class DataType{
+	SOF,
+	LENGTH_LOW,
+	LENGTH_HIGH,
+	CMD0,
+	CMD1,
+	PAYLOAD,
+	FCS
+};
+DataType mType;
+unsigned short payIndex;
+format mProData;
+};
 std::promise<bool> quitPromise;
 class DataCollector : public oym::SerialListener
 {
 public:
-
 	void onClosed()
 	{
 		std::cout << "[INFO]: Serial disconnected" << std::endl;
@@ -19,10 +108,28 @@ public:
 
 	void onData(unsigned char data)
 	{
-		std::cout << static_cast<unsigned int>(data) << " ";
-		if (data == 10 || data == 20)
-			std::cout << "\n";
+		std::cout << std::hex << static_cast<int>(data) << " ";
+		auto ret = mProtoco.parseData(data);
+		if(ret){
+			auto value = ret.value();
+			if (value.payload.size() == 0)
+				return;
+			if(value.cmd1 == 0x05 && value.payload[0] == 0x02){
+				std::cout<<"[INFO]: connection termination event"<<std::endl;
+			} else if(value.cmd1 == 0x05 && value.payload[0] == 0x01){
+				std::cout<<"[INFO]: connect establishment event"<<std::endl;
+			}
+			else if (value.cmd1 == 0x05 && value.payload[0] == 0x08) {
+				std::cout << "[INFO]: advertising" << std::endl;
+			}
+			else if (value.cmd1 == 0x89 && value.payload[0] != 00) {
+				std::cout << "[ERROR]: Notification reponse error:	"
+					<<value.payload[0] << std::endl;
+			}
+		}
 	}
+private:
+	DataProtocl mProtoco;
 };
 
 int main(int argc, char** argv)
@@ -34,24 +141,18 @@ int main(int argc, char** argv)
 	std::cout << "Please input serial number: ";
 	std::cin >> comport;
 	std::cout << "comport:	"<<	comport << std::endl;
-	auto ret = adapter.open(comport);
+	auto ret = adapter.open(comport,115200UL);
 	if (ret != oym::SUCCESS) {
 		std::cout << "Open Serial Port failure\n";
 		return 0;
+	}
+	else {
+		std::cout << "Open serial port success" << std::endl;
 	}
 	std::this_thread::sleep_for(std::chrono::seconds(2));
 	auto fu = quitPromise.get_future();
 	while (std::future_status::timeout ==
 			fu.wait_for(std::chrono::milliseconds(100))){
-		adapter.write_sync(0x01);
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-		auto prt1 = std::make_shared<std::vector<unsigned char>>(
-			std::initializer_list<unsigned char>{2,3,4,5,6,7,8,9,10});
-		if (oym::FAILURE == adapter.write_sync(prt1))
-			std::cout << "send failure" << std::endl;
-		adapter.write_async(11);
-		std::iota(prt1->begin(), prt1->end(), 12);
-		adapter.write_async(prt1);
 	}
 	return 0;
 }
